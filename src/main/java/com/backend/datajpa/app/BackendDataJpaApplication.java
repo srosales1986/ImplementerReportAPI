a90b98c7dcece34417c69391e7664421fb8c1f1f
Sebastian R <sebastianrosales.correo@gmail.com>
2020-08-09 01:19:25 -0300

    First commit ApiRest report

---
+++ src/main/java/com/backend/datajpa/app/BackendDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.datajpa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDataJpaApplication.class, args);
+	}
+
+}
